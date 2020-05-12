65c521e354fd54e118ed92f4bf7b2dd631caf047
George Davis <george_davis@trendmicro.com>
2020-05-12 01:13:16 -0400

    Adding APIs and a MySQL Backend

--- lab_3/app/index.js
+++ lab_3/app/index.js
@@ -2,16 +2,31 @@ require('trend_app_protect');
 
 const express = require("express");
 
+const mysql = require('./services/mysql/mysql-client.js')
+
 const port = 8080;
 
 var app = express();
 
 app.use(express.static("public"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/test", (req, res) => {
   res.status(200).send("Hello, World!");
 });
 
+app.get("/allUserEntries", (req, res) => {
+  mysql.getAllUserEntries();
+  res.status(200).send("Hello, User Entries!");
+});
+
+app.post("/userEntry", (req, res) => {
+  mysql.postUserEntry(req.body.user_entry);
+  res.status(200).send("Hello, New Entry!");
+});
+
 module.exports = app.listen(port, () => {
-  process.stdout.write("Listening on port " + port);
+  process.stdout.write("Listening on port " + port + "\n");
 });
---
+++ lab_3/app/services/mysql/mysql-client.js
@@ -0,0 +1,38 @@
+var mysql = require('mysql');
+
+var DB_HOST = process.env.DB_HOST;
+var DB_USER = process.env.DB_USER;
+var DB_PASSWORD = process.env.DB_PASSWORD;
+var DB_NAME = process.env.DB_NAME;
+
+var connection = mysql.createConnection({
+  host     : DB_HOST,
+  user     : DB_USER,
+  password : DB_PASSWORD,
+  database : DB_NAME
+});
+
+//connect to database
+connection.connect((err) =>{
+    if(err) throw err;
+    console.log('\n\n\tMySQL Connected!');
+});
+
+exports.getAllUserEntries = () => {
+ 
+    connection.query('SELECT user_entries FROM tbl_user_entries', function (error, results, fields) {
+        if (error) throw error;
+        if (results.length)
+            console.log('\n\n\tThe solution is: ', results[0].user_entries);
+    });
+    
+};
+
+exports.postUserEntry = (user_entry) => {
+ 
+    connection.query("INSERT INTO tbl_user_entries (user_entries) VALUES (" + user_entry + ");", function (error, results, fields) {
+        if (error) throw error;
+        console.log('\n\n\tInserted : ' + user_entry);
+    });
+
+};
